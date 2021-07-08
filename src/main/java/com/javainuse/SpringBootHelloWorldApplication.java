05e5af7a6f8a3df2f7b2a97edbf1a538ada9134e
govarthinisundararaj <35028923+govarthinisundararaj@users.noreply.github.com>
2021-07-08 18:30:21 +0530

    Update SpringBootHelloWorldApplication.java

--- src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
+++ src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
@@ -6,6 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootHelloWorldApplication {
 
+	
+	@Override
+5
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+6
+        return application.sources(SpringBootHelloWorldApplication.class);
+7
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
 	}
